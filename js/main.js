556c67c4541669bc0097dc776cf188e05f0efbeb
Eduardo Salinas <eduardoluis93@gmail.com>
2022-07-06 11:30:13 +0100

    BUG FIX: Fixed Google Analytics' Tag Bug
    I fixed a bug in which the tag code from Google Analytics would only execute on the home page the 1st time that they entered the page, but then the code would never run again.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ const cookieButton = document.querySelector(".cookie-btn");
 I decided to add here Google Analytic's tag so that I will only take users' private data if they
 click on "I accept", so that I don't take their private data if they don't accept the cookies. Google's tag is just
 another type of cookie.
+
+I ended up removing the Google Analytics code from here since it wil only execute the 1st time the user clicks on "I accept".
+That's bad since I will only be able to track user information if the user eneters the 1st time on the home page, but not whenever 
+he visits another page withing my site. 
 */
 cookieButton.addEventListener("click", () => {
 
@@ -18,11 +22,11 @@ cookieButton.addEventListener("click", () => {
     localStorage.setItem("cookieBannerDisplayed", "true");
 
     // Tag from Google Analytics (cookie), which takes info from the sers who enter the site
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    // window.dataLayer = window.dataLayer || [];
+    // function gtag(){dataLayer.push(arguments);}
+    // gtag('js', new Date());
   
-    gtag('config', 'G-2YRG54NPH3');
+    // gtag('config', 'G-2YRG54NPH3');
 });
 
 setTimeout(() => {
@@ -30,3 +34,18 @@ setTimeout(() => {
         cookieContainer.classList.add("active");
     }
 }, 2000);       /* End of the cookie consent banner JS code. */
+
+/* Tag from Google Analytics.
+
+This will always execute if the user has the cookieBannerDisplayed cookie. That is, this will always run if the user had
+previously clicked on "I accept" on the cookie consent banner.
+*/
+if (localStorage.getItem("cookieBannerDisplayed")) {
+    
+    // Tag from Google Analytics (cookie), which takes info from the sers who enter the site
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-2YRG54NPH3');
+}
