d05599ad61c66fcdd6ef6d4f26b350ef982acdb0
Eduardo Salinas <eduardoluis93@gmail.com>
2022-07-06 11:06:37 +0100

    Added Google Analytic's Tag With Cookie Consent
    I readded the tag from Google Analytics to track info about users, but it will only work if users click on the 'I Accept' button from the cookie consetn banner.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,26 @@
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
 
+/* This will execute if the user clicks on "I accept" on the cookie banner.
+
+I decided to add here Google Analytic's tag so that I will only take users' private data if they
+click on "I accept", so that I don't take their private data if they don't accept the cookies. Google's tag is just
+another type of cookie.
+*/
 cookieButton.addEventListener("click", () => {
+
+    // This hides the cookie banner after clicking on "I accept"
     cookieContainer.classList.remove("active");
+
+    // This adds a cookie
     localStorage.setItem("cookieBannerDisplayed", "true");
+
+    // Tag from Google Analytics (cookie), which takes info from the sers who enter the site
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-2YRG54NPH3');
 });
 
 setTimeout(() => {
