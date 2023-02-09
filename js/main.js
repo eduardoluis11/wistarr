66a9050c512699c59bd0d4c6a6b3677a098b867a
Eduardo Salinas <eduardoluis93@gmail.com>
2023-02-09 19:59:59 +0000

    Portfolio Videos Appear After Accepting Cookies
    The portfolio videos from YouTube will only show up if the user accepts the website's cookies.

--- js/main.js
+++ js/main.js
@@ -46,18 +46,40 @@ setTimeout(() => {
 
 This will always execute if the user has the cookieBannerDisplayed cookie. That is, this will always run if the user had
 previously clicked on "I accept" on the cookie consent banner.
+
+To make the website GDPR compliant, and to prevent the YouTube cookies from appearing until after accepting the cookie consent banner, 
+I will add the YouTube links to the portfolio videos by using setAttribute.
+
 */
 if (localStorage.getItem("cookieBannerDisplayed")) {
 
     // DEBUG msg
     console.log("Google Analytics is taking your data.")
     
-    // Tag from Google Analytics (cookie), which takes info from the sers who enter the site
+    // Tag from Google Analytics (cookie), which takes info from the users who enter the site
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
   
     gtag('config', 'G-2YRG54NPH3');
+
+    // This selects the 1st iFrame tag of my portfolio page
+    video_1 = document.getElementById("portfolio_video_1")
+
+    // This inserts the link of the 1st video into the iFrame of the 1st portfolio video
+    video_1.setAttribute("src", "https://www.youtube-nocookie.com/embed/UJHt2U5h5fs");
+
+    // This selects the 2nd iFrame tag of my portfolio page
+    video_2 = document.getElementById("portfolio_video_2")
+
+    // This inserts the link of the 1st video into the iFrame of the 2nd portfolio video
+    video_2.setAttribute("src", "https://www.youtube-nocookie.com/embed/2ff_mKe0rmg");
+
+    // This selects the 3rd iFrame tag of my portfolio page
+    video_3 = document.getElementById("portfolio_video_3")
+
+    // This inserts the link of the 1st video into the iFrame of the 3rd portfolio video
+    video_3.setAttribute("src", "https://www.youtube-nocookie.com/embed/6bNyVyBNnqs")
 
 // This else will only display a DEBUG msg if the user doesn't click on "I accept"
 } else {
